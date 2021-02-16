f4c0912c027e6897f9b7d3addcb47da969832add
Andrew <ramettaandrewj@gmail.com>
2021-02-16 06:02:09 -0500

    added tests and xss

--- src/app.js
+++ src/app.js
@@ -4,14 +4,20 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const articlesRouter = require("./articles/article-router");
 
 const app = express();
 
-const morganOption = (NODE_ENV === "production" ? "tiny" : "common");
-
-app.use(morgan(morganOption));
+app.use(morgan(NODE_ENV === "production" ? "tiny" : "common"));
 app.use(cors());
 app.use(helmet());
+
+app.use("/api/articles", articlesRouter);
+
+app.get("/xss", (req, res) => {
+  res.cookie("secretToken", "1234567890");
+  res.sendFile(__dirname + "/xss-example.html");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -19,10 +25,10 @@ app.get("/", (req, res) => {
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
-  if (NODE_ENV === "product") {
-    response = { error: { message: "server error" } };
+  if (NODE_ENV === "production") {
+    response = { error: "Server error" };
   } else {
-    console.log(error);
+    console.error(error);
     response = { message: error.message, error };
   }
   res.status(500).json(response);
